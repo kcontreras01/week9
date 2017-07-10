c27a4d209a69c5e1a10617624776b25f1199c98c
kcontreras01 <kiara@Kiaras-MacBook-Pro.local>
2017-07-10 19:44:15 -0400

    First

--- js/project.js
+++ js/project.js
@@ -3,11 +3,21 @@ $(".gallery").masonry({
   columnWidth: ".thumbnail",
   percentPosition: true
 });
+// creates a responsive layout
 
-$(".touchevents .thumbnail").hammer().bind("press", function() {
-// Your code here
-});
 
-$(".touchevents .thumbnail").hammer().bind("swipe", function() {
-// Your code here
-});
+// $(".touchevents .thumbnail").hammer().bind("press", function() {
+// 	var query = Modernizr.mq('(max-width: 800px)');
+// 	if (query) {
+// 		$('.thumbnail').addClass('.open')
+// 	}
+// });
+
+// $(".touchevents .thumbnail").hammer().bind("swipe", function() {
+// 	var query = Modernizr.mq('(max-width: 800px)');
+// 	if (query) {
+// 		$('.thumbnail').removeClass('.open')
+// 	}
+// });
+
+$('.thumbnail a').simpleLightbox( { overlay: false, closeText: 'X'});
